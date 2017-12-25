d5a9373b67ad4e0c0699d083c81261ae7d4380b3
junesponge <junes.yu@qq.com>
2017-12-25 16:31:29 +0800

    Create module for Service Feign Client.

---
+++ service-feign/src/main/java/com/springcloud/demo/servicefeign/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.springcloud.demo.servicefeign.controller;
+
+import com.springcloud.demo.servicefeign.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/sayViaFeignClient", method = RequestMethod.GET)
+    public String sayViaFeignClient(@RequestParam String word) {
+        return helloService.sayViaFeignClient(word);
+    }
+}
---
+++ service-feign/src/main/java/com/springcloud/demo/servicefeign/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.springcloud.demo.servicefeign.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "EUREKA-CLIENT")
+public interface HelloService {
+
+    @RequestMapping(value = "/say", method = RequestMethod.GET)
+    String sayViaFeignClient(@RequestParam(name = "word") String word);
+
+}
