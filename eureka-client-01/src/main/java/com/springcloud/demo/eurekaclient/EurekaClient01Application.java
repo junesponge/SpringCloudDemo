30c8b41b575522de4b050a7e09eb56a308d0a115
junesponge <junes.yu@qq.com>
2017-12-31 12:25:51 +0800

    Client 02 call client 01 via Restful.

--- eureka-client-01/src/main/java/com/springcloud/demo/eurekaclient/EurekaClient01Application.java
+++ eureka-client-01/src/main/java/com/springcloud/demo/eurekaclient/EurekaClient01Application.java
@@ -7,6 +7,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -23,5 +27,13 @@ public class EurekaClient01Application {
 	@RequestMapping("/say")
 	public String say(@RequestParam String word) {
 		return "I say '" + word + "' from port:" + port;
+	}
+
+	@RequestMapping("/request")
+	public String request(@RequestParam String param) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		System.out.println("URI: " + request.getRequestURI());
+		System.out.println("Param: " + param);
+		return "Request from Port: " + request.getRemotePort();
 	}
 }
--- eureka-client-02/src/main/java/com/springcloud/demo/eurekaclient/EurekaClient02Application.java
+++ eureka-client-02/src/main/java/com/springcloud/demo/eurekaclient/EurekaClient02Application.java
@@ -1,5 +1,9 @@
 package com.springcloud.demo.eurekaclient;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +11,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -23,5 +37,21 @@ public class EurekaClient02Application {
 	@RequestMapping("/say")
 	public String say(@RequestParam String word) {
 		return "I say '" + word + "' from port:" + port;
+	}
+
+	@RequestMapping("/callClient01")
+	public String callClient01(@RequestParam String word) throws IOException {
+		HttpClient httpClient = HttpClients.createDefault();
+		HttpResponse response = httpClient.execute(new HttpGet("http://localhost:8762/request?param=" + word));
+		// 获取返回消息
+		BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		StringBuffer sb = new StringBuffer("");
+		String line = "";
+		String NL = System.getProperty("line.separator");
+		while ((line = in.readLine()) != null) {
+			sb.append(line + NL);
+		}
+		in.close();
+		return sb.toString();
 	}
 }
