692498a059ad317feac6d4bcbbba4ff586bd2651
junesponge <junes.yu@qq.com>
2018-01-01 23:26:11 +0800

    Update the service ID to lowercase.

--- service-ribbon/src/main/java/com/springcloud/demo/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/springcloud/demo/serviceribbon/service/HelloService.java
@@ -3,6 +3,7 @@ package com.springcloud.demo.serviceribbon.service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -13,7 +14,13 @@ public class HelloService {
 
     @HystrixCommand(fallbackMethod = "hitError") // 指定断路器调用方法
     public String sayViaRibbonClient(String word) {
-        return restTemplate.getForObject("http://EUREKA-CLIENT/say?word=" + word, String.class);
+        String response = null;
+        try {
+            response = restTemplate.getForObject("http://eureka-client/say?word=" + word, String.class);
+        } catch (RestClientException e) {
+            e.printStackTrace();
+        }
+        return response;
     }
 
     public String hitError(String word) {
