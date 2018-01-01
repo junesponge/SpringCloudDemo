6d0507c21eb11d616ca7b36c3477074e60fdbbd4
junesponge <junes.yu@qq.com>
2018-01-01 19:12:43 +0800

    Create the Spring Cloud Config Server & Client Demo module for testing.

---
+++ config-client/src/main/java/com/springcloud/demo/configclient/ConfigClientApplication.java
@@ -0,0 +1,19 @@
+package com.springcloud.demo.configclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Autowired
+	void setEnviroment(Environment env) {
+		System.out.println("service-zuul from env: " + env.getProperty("service-zuul.appName"));
+	}
+}
---
+++ config-server/src/main/java/com/springcloud/demo/configserver/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.springcloud.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * http请求地址和资源文件映射如下:
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
