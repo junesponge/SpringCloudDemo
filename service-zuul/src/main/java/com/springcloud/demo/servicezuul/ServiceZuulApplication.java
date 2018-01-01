fd50f2ce71d5f97dfc1edfbc89ddcdeec9727dd9
junesponge <junes.yu@qq.com>
2018-01-01 19:09:22 +0800

    Reform the service-zuul module to a spring cloud config-client, and create a config-repository for store the config for each module in each environment.

--- service-zuul/src/main/java/com/springcloud/demo/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/springcloud/demo/servicezuul/ServiceZuulApplication.java
@@ -1,9 +1,11 @@
 package com.springcloud.demo.servicezuul;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.env.Environment;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -12,5 +14,10 @@ public class ServiceZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+
+	@Autowired
+	void setEnviroment(Environment env) {
+		System.out.println("service-zuul from env: " + env.getProperty("service-zuul.appName"));
 	}
 }
